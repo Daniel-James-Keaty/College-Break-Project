fc1d2b71fa644daa1acf3bd4299303ce7c500c39
Daniel James Keaty <danielkeaty@protonmail.ch>
2024-04-04 16:11:52 +0100

    Changes to be committed: renamed:    "print (\"Hello World\").py" -> College_Break_Project.py

--- College_Break_Project.py
+++ College_Break_Project.py
@@ -5,6 +5,6 @@
 print ("Hello World")
 
 # Okay so I am doing the project for the college break. 
-#It will be submitted for graded next week. Im using VScode on Fedora Linux, and Github for the first time. 
+# It will be submitted for graded next week. Im using VScode on Fedora Linux, and Github for the first time. 
 # The learning curve is steep but hopefully, itl be worth it lol
 
