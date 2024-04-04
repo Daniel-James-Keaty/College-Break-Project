c149e35acfe6f48a89ebf99c0d9462ad09a03056
Daniel James Keaty <danielkeaty@protonmail.ch>
2024-04-04 16:43:03 +0100

    Made some initial changes. Changed the name of the main file.

---
+++ main.py
@@ -0,0 +1,35 @@
+# Name: Daniel James Keaty
+# Purpose: Modular Programming Project 
+# Date: 04 - April - 2024
+
+print ("Hello World")
+
+# Okay so I am doing the project for the college break. 
+# It will be submitted for graded next week. Im using VScode on Fedora Linux, and Github for the first time. 
+# The learning curve is steep but hopefully, itl be worth it lol
+
+# I made a flow chart last week outlining how I aim to tackle the project and how it should work
+
+def main():
+    # Initial introduction
+    print ("Welcome to Dan's Flight Management system!")
+    print("Please select an option from the menu below")
+
+
+    while True:
+    # Display Menu Options
+        print("\n Menu")
+        print("1) Book a Flight")
+        print("2) Review Booking")
+        print("3) Manage Passenger Details")
+        print("4) Exit")
+        
+        # Handle User Input
+        user_menu_selection = int(input("Please select an option (1-4): "))
+        
+        
+
+    #print ("Please select an option;")
+
+main()
+
