560115db0ebbbd767f8f0489758f99f460ac5cfb
Daniel James Keaty <danielkeaty@protonmail.ch>
2024-04-04 18:23:54 +0100

    Trying to impliment function to review bookings

--- main.py
+++ main.py
@@ -2,29 +2,35 @@
 # Purpose: Modular Programming Project 
 # Date: 04 - April - 2024
 
-print ("Hello World")
+# print ("Hello World")
+# I didnt realise this was still in here lol. Carry over from when I made the file and wanted to check everything was working.
 
 # Okay so I am doing the project for the college break. 
 # It will be submitted for graded next week. Im using VScode on Fedora Linux, and Github for the first time. 
-# The learning curve is steep but hopefully, itl be worth it lol
+# The learning curve is steep but hopefully, itl be worth it lol.
 
-# I made a flow chart last week outlining how I aim to tackle the project and how it should work
+# I made a flow chart last week outlining how I aim to tackle the project and how it should work. That is also on the Github page. 
+
+import os
+# Need this for checking if bookings.txt exists in the same directory as this file.
+
 
 def main():
-    # Initial introduction
+    # Creating main function.
+    # Initial introduction.
     print ("Welcome to Dan's Flight Management system!")
     print("Please select an option from the menu below")
 
 
     while True:
-    # Display Menu Options
+    # Display Menu Options.
         print("\n Menu")
         print("1) Book a Flight")
         print("2) Review Booking")
         print("3) Manage Passenger Details")
         print("4) Exit")
         
-        # Handle User Input
+        # Handle User Input.
         user_menu_selection = int(input("Please select an option (1-4): "))
         
         if user_menu_selection == 1:
@@ -33,7 +39,7 @@ def main():
             break
             
         elif user_menu_selection == 2:
-            print ("Placeholder where function 2 goes")
+            review_booking_function
             
             break
             
@@ -43,18 +49,56 @@ def main():
             break
             
         elif user_menu_selection == 4:
-            # Exit program
+            # Exits program.
             print ("Exiting Program...")
             
             break
             
         else:
-            # Handles Invalid choices
+            # Handles Invalid choices.
             print("Invalid choice. Please try again.")
             
 
 
     #print ("Please select an option;")
 
+def review_booking_function():
+    # Creating the fuction for option 2 in the menu. 
+    # Will be able to review bookings and interact with bookings.txt file.
+    
+    # From what I understand, this uses import.os to grab current directory. 
+    current_directory = os.path.dirname(os.path.realpath(__file__))
+    
+    # Uses the same thing to grab the path to the bookings.txt file. 
+    booking_file_path = os.path.join(current_directory, "bookings.txt")
+    
+    print("Booking function found, yay!")
+    # Currently having trouble as this function is being called properly. 
+    
+    
+    if os.path.isfile(booking_file_path):
+        print("Booking file found...initialising!")
+        
+    else:
+        print("Booking file not found...creating file now")
+        with open(booking_file_path, "w") as file:
+            pass
+        print("File has now been created!")
+        
+        
+    
+    
+    
+    input(int("Please enter your booking number: "))
+    print("Thank you.")
+    
+    
+    
+    
+
+
+
 main()
+
+
 
